11f313ab1c7ec7b4d9f6bf36849459772c0288f4
NajibxTheJib <138123772+NajibxTheJib@users.noreply.github.com>
2023-06-29 14:52:27 +0200

    Project 1 - Answer business questions.sql

--- Project 1 - Answer business questions.sql
+++ Project 1 - Answer business questions.sql
@@ -12,6 +12,8 @@ We determinate the following products as category Tech :
 'pc_gamer' 
 'telephony'
 
+'audio', 'cine_foto', 'consoles_games', 'eletronicos', 'informatica_acessorios', 'pc_gamer', 'pcs', 'telefonia'
+
 */
 
 
@@ -23,18 +25,24 @@ represent from the overall number of products sold?
 
 15584 rows returned for the tech category sells
 
-Percentage = (15584 / 112650) x 100
-Percentage = 13,83 %
+Percentage = (16924 / 112650) x 100
+Percentage = 15,23 %
+
+Audio :364
+cine_foto : 72
+consoles_games : 1137
+eletronicos : 2767
+informatica_acessorios : 7827
+pc_gamer : 9
+pcs : 203
+telefonia : 4545
 
 */
 
 SELECT
-	product_id,
-    product_category_name
+	COUNT(*)
 FROM
-	products
-GROUP BY
-	product_id
+	order_items
 ;
 	
 SELECT 
@@ -50,6 +58,24 @@ JOIN product_category_name_translation
 	ON products.product_category_name = product_category_name_translation.product_category_name
 ;
 
+
+SELECT 
+	orders.order_id, 
+    order_items.product_id , 
+    products.product_category_name , 
+    product_category_name_translation.product_category_name_english
+FROM 
+	order_items
+LEFT JOIN 
+	orders 
+		ON orders.order_id = order_items.order_id
+LEFT JOIN products 
+	ON order_items.product_id = products.product_id
+LEFT JOIN product_category_name_translation 
+	ON products.product_category_name = product_category_name_translation.product_category_name 
+;
+
+    
 SELECT 
 	orders.order_id, 
     order_items.product_id , 
@@ -67,6 +93,101 @@ JOIN product_category_name_translation
 WHERE
 	product_category_name_translation.product_category_name_english IN ('audio', 'cine_photo', 'console_games', 'electronics', 'computer', 'computers_accessories', 'pc_gamer', 'telephony')
 ;
+
+SELECT 
+	orders.order_id, 
+    order_items.product_id , 
+    products.product_category_name , 
+    product_category_name_translation.product_category_name_english
+FROM 
+	order_items
+LEFT JOIN 
+	orders 
+		ON orders.order_id = order_items.order_id
+LEFT JOIN products 
+	ON order_items.product_id = products.product_id
+LEFT JOIN product_category_name_translation 
+	ON products.product_category_name = product_category_name_translation.product_category_name 
+WHERE
+	product_category_name_translation.product_category_name_english IN ('audio', 'cine_photo', 'console_games', 'electronics', 'computer', 'computers_accessories', 'pc_gamer', 'telephony')
+    
+;
+
+
+
+SELECT
+    products.product_category_name,
+    COUNT(*)
+FROM
+    order_items
+LEFT JOIN
+    orders
+        ON orders.order_id = order_items.order_id
+LEFT JOIN
+    products
+        ON order_items.product_id = products.product_id
+LEFT JOIN
+    product_category_name_translation
+        ON products.product_category_name = product_category_name_translation.product_category_name
+WHERE
+    product_category_name_translation.product_category_name_english IN ('audio', 'cine_photo', 'console_games', 'electronics', 'computer', 'computers_accessories', 'pc_gamer', 'telephony')
+GROUP BY
+    products.product_category_name
+;
+
+SELECT 
+    DISTINCT products.product_category_name,
+    COUNT(products.product_category_name) AS Count
+FROM 
+	order_items
+LEFT JOIN 
+	orders 
+		ON orders.order_id = order_items.order_id
+LEFT JOIN products 
+	ON order_items.product_id = products.product_id
+WHERE
+	 products.product_category_name IN ('audio', 'cine_foto', 'consoles_games', 'eletronicos', 'informatica_acessorios', 'pc_gamer', 'pcs', 'telefonia')
+GROUP BY 
+	products.product_category_name
+;
+
+SELECT 
+    COUNT(products.product_category_name) AS Total
+FROM 
+	order_items
+LEFT JOIN 
+	orders 
+		ON orders.order_id = order_items.order_id
+LEFT JOIN products 
+	ON order_items.product_id = products.product_id
+WHERE
+	 products.product_category_name IN ('audio', 'cine_foto', 'consoles_games', 'eletronicos', 'informatica_acessorios', 'pc_gamer', 'pcs', 'telefonia')
+;
+
+SELECT 
+	((SELECT 
+    COUNT(products.product_category_name) AS Total
+FROM 
+	order_items
+LEFT JOIN 
+	orders 
+		ON orders.order_id = order_items.order_id
+LEFT JOIN products 
+	ON order_items.product_id = products.product_id
+WHERE
+	 products.product_category_name IN ('audio', 'cine_foto', 'consoles_games', 'eletronicos', 'informatica_acessorios', 'pc_gamer', 'pcs', 'telefonia')
+)
+/
+(SELECT
+	COUNT(*)
+FROM
+	order_items
+    ))
+* 100
+AS sales_percentage_tech
+;
+
+
 
 /*
 
@@ -95,7 +216,123 @@ FROM
 
 Are expensive tech products popular?
 
+We definied as an expensive product, a product of price of 300 euros or more
+=> 910 expensive products on the total of the 16924 sell products (Approx = 5,37%)
+
 */
 
+SELECT 
+	COUNT(*) AS count,
+CASE
+	WHEN price > 300 then 'expensive'
+    WHEN price <= 300 and price > 100 then 'not so expensive'
+    else 'cheap'
+    end as price_category
+FROM 
+	magist.order_items
+LEFT JOIN products 
+	ON order_items.product_id = products.product_id
+LEFT JOIN product_category_name_translation 
+	ON products.product_category_name = product_category_name_translation.product_category_name 
+WHERE
+	product_category_name_translation.product_category_name_english IN ('audio', 'cine_photo', 'console_games', 'electronics', 'computer', 'computers_accessories', 'pc_gamer', 'telephony')
+GROUP BY
+	price_category
+ORDER BY
+	price DESC
+;	
 
+SELECT 
+    price_category,
+    COUNT(*) AS count
+FROM (
+    SELECT 
+	 order_id,
+     order_item_id,
+     price,
+CASE
+	WHEN price > 300 then 'expensive'
+    WHEN price <= 300 and price > 100 then 'not so expensive'
+    else 'cheap'
+    end as price_category
+FROM 
+	magist.order_items
+LEFT JOIN products 
+	ON order_items.product_id = products.product_id
+WHERE
+	products.product_category_name IN ('audio', 'cine_foto', 'consoles_games', 'eletronicos', 'informatica_acessorios', 'pc_gamer', 'pcs', 'telefonia')
+) AS subquery
+GROUP BY 
+	price_category
+ORDER BY 
+	count DESC
+;
 
+SELECT 
+    price_category,
+    COUNT(*) AS count
+FROM (
+    SELECT 
+	 order_id,
+     order_item_id,
+     price,
+CASE
+	WHEN price > 300 then 'expensive'
+    WHEN price <= 300 and price > 100 then 'not so expensive'
+    else 'cheap'
+    end as price_category
+FROM 
+	magist.order_items
+LEFT JOIN products 
+	ON order_items.product_id = products.product_id
+WHERE
+	products.product_category_name IN ('audio', 'cine_foto', 'consoles_games', 'eletronicos', 'informatica_acessorios', 'pc_gamer', 'pcs', 'telefonia')
+AND price > 300
+) AS subquery
+GROUP BY 
+	price_category
+;
+
+SELECT
+	((SELECT 
+    COUNT(*) AS count
+FROM (
+    SELECT 
+	 order_id,
+     order_item_id,
+     price,
+CASE
+	WHEN price > 300 then 'expensive'
+    WHEN price <= 300 and price > 100 then 'not so expensive'
+    else 'cheap'
+    end as price_category
+FROM 
+	magist.order_items
+LEFT JOIN products 
+	ON order_items.product_id = products.product_id
+WHERE
+	products.product_category_name IN ('audio', 'cine_foto', 'consoles_games', 'eletronicos', 'informatica_acessorios', 'pc_gamer', 'pcs', 'telefonia')
+AND price > 300
+) AS subquery
+GROUP BY 
+	price_category
+)
+/
+(SELECT 
+    COUNT(products.product_category_name) AS Total
+FROM 
+	order_items
+LEFT JOIN 
+	orders 
+		ON orders.order_id = order_items.order_id
+LEFT JOIN products 
+	ON order_items.product_id = products.product_id
+WHERE
+	 products.product_category_name IN ('audio', 'cine_foto', 'consoles_games', 'eletronicos', 'informatica_acessorios', 'pc_gamer', 'pcs', 'telefonia')
+))
+*
+100
+AS percentage
+;
+
+SELECT
